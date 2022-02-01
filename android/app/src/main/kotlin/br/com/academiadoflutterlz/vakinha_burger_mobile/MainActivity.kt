7592bd897eabd9eba7fd540c0dd8fa4488149a56
Luciano Zimermann <lucianozn16@gmail.com>
2022-01-31 22:55:40 -0300

    Setting app themes

---
+++ android/app/src/main/kotlin/br/com/academiadoflutterlz/vakinha_burger_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.academiadoflutterlz.vakinha_burger_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/vakinha_ui.dart';
+import 'package:vakinha_burger_mobile/app/routes/splah_routers.dart';
+
+void main() {
+  runApp(const VakinhaBurgerMainApp());
+}
+
+class VakinhaBurgerMainApp extends StatelessWidget {
+  const VakinhaBurgerMainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Vakinha Burger',
+      theme: VakinhaUI.theme,
+      getPages: [
+        ...SplashRouters.routers,
+      ],
+    );
+  }
+}
+
