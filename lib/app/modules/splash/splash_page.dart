45510c0255041a1e2f82becda10dcb088440c492
Luciano Zimermann <lucianozn16@gmail.com>
2022-01-31 23:46:32 -0300

    Creating text form field

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_textformfield.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -6,10 +7,18 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
+      appBar: AppBar(title: const Text('Splash')),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: VakinhaTextFormField(label: 'Text Form'),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
